288dc59a7da826b9137ae9e370fa3bec55f688ba
tejasM17 <tejasm9380@gmail.com>
2026-02-13 09:59:05 +0530

    Hello, world!

---
+++ src/main/kotlin/org/example/hello.kt
@@ -0,0 +1,7 @@
+package org.example
+fun main() {
+    var x = 5
+    var y = 2
+
+    println(x * y)
+}
---
+++ src/main/kotlin/org/example/two.kt
@@ -0,0 +1,8 @@
+package org.example
+
+fun main() {
+    val name  = "tejas"
+    val age = 19
+
+    println("Am $name and am $age years old")
+}
