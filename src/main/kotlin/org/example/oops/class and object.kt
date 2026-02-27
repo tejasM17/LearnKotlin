f9988453cb207f0d2e1e79d3882792fbe917dc9d
tejasM17 <tejasm9380@gmail.com>
2026-02-27 12:03:36 +0530

    Class objects

---
+++ src/main/kotlin/org/example/oops/class and object.kt
@@ -0,0 +1,15 @@
+package org.example.oops
+
+class `class and object` {
+    var color = "Red"
+
+    fun drive() {
+        println("Car is driving")
+    }
+}
+
+
+fun main() {
+    val newCar = `class and object`()
+    newCar.drive()
+}
